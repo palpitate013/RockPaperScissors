381151e83e010f3eb7d6111e72cd654722cbb2bc
palpitate013 <github.palpitate013@passmail.net>
2024-10-02 17:36:25 -0400

    Introduction ask user if they would like to play

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 // Introduction "Would you like to play"
-console.log("Hello! would you like to play rock, paper, scissors?")
-// if yes go to play function
-// if no return "darn..." return to intro
-// if else "please enter y or n" and wait for response
-
-// play function
-// User points = 0
-// Computer points = 0
-// Generate outcome
-// Rock < Paper < Scissors
-// Ask user for choice and wait for response
-// Compare choice to outcome
-// If outcome is greater then choice call function lose
-    // computer points ++
-// If choice is greater then outcome call function win
-    // user points ++
-// If else
-    // I must be high lets try again wait for response
+let answer = prompt("Hello! would you like to play rock, paper, scissors?");
+if (answer = "y"){
+    console.log("Alright, lets get started");
+}else if (answer = "n"){
+    console.log("Darn you got my hopes up");
+}else {
+    console.log('Sorry I must be high. Try "y" or "n"');
+}
