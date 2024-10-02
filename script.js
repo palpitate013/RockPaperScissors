99fe61bd7dc488acfa10e46bb2f126b502ccbbcf
palpitate013 <github.palpitate013@passmail.net>
2024-10-02 17:59:54 -0400

    Set up basic game interface.

--- script.js
+++ script.js
@@ -2,6 +2,17 @@
 let answer = prompt("Hello! would you like to play rock, paper, scissors?");
 if (answer = "y"){
     console.log("Alright, lets get started");
+    let choice = prompt("Your choice: ");
+    sleep(1000);
+    console.log("Ready...")
+    sleep(1000);
+    console.log("Rock...")
+    sleep(1000);
+    console.log("Paper...")
+    sleep(1000);
+    console.log("Scissors...")
+    sleep(1000);
+    console.log("shoot...")
 }else if (answer = "n"){
     console.log("Darn you got my hopes up");
 }else {
